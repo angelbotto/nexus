15453d255da01447808bbc4ff67ecc9f60fdca81
Angel Botto <angel@botto.is>
2026-03-19 16:13:43 -0500

    feat(04-01): add LRU order tracking to AppState
    - Add VecDeque import alongside HashSet
    - Add LRU_POOL_SIZE = 8 constant
    - Add lru_order: VecDeque<String> field to AppState
    - Initialize lru_order: VecDeque::new() in AppState::new

--- src-tauri/src/state.rs
+++ src-tauri/src/state.rs
@@ -1,12 +1,15 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, VecDeque};
 
 use crate::config::NexusConfig;
+
+pub const LRU_POOL_SIZE: usize = 8;
 
 pub struct AppState {
     pub config: NexusConfig,
     pub active_app_id: Option<String>,
     pub webviews_created: HashSet<String>,
     pub sidebar_visible: bool,
+    pub lru_order: VecDeque<String>,
 }
 
 impl AppState {
@@ -17,6 +20,7 @@ impl AppState {
             active_app_id: None,
             webviews_created: HashSet::new(),
             sidebar_visible,
+            lru_order: VecDeque::new(),
         }
     }
 }
